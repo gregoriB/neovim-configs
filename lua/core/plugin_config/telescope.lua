724d00b0195ba6492fcc97b14e89a69f0766a427
gregoriB <brandon.gregori@gmail.com>
2023-07-03 20:56:18 -0400

    Update telescope to sort by recently used buffers

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,8 +1,6 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-builtin.buffers({ sort_mru=true, ignore_current_buffer=true })
-
 vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
 vim.keymap.set('n', '<leader><CR>', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader><', builtin.buffers)
@@ -16,5 +14,11 @@ vim.keymap.set('n', '<leader>cD', builtin.lsp_type_definitions, {})
 telescope.setup{
   defaults = require('telescope.themes').get_ivy {
     theme = "ivy",
+  },
+  pickers = {
+    buffers = {
+      sort_mru = true,
+      ignore_current_buffer = true,
+    }
   }
 }
