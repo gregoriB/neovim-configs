3019f3a18a405c37247c9890ca1322ea3287722a
gregoriB <brandon.gregori@gmail.com>
2024-07-28 13:01:12 -0400

    Add go dap config

--- lua/core/plugin_config/dap_config.lua
+++ lua/core/plugin_config/dap_config.lua
@@ -2,56 +2,56 @@ local dap = require("dap")
 local dapui = require("dapui")
 
 dap.adapters.codelldb = {
-  type = "server",
-  port = 13000,
-  executable = {
-    command = vim.fn.expand('~/.local/share/nvim/mason/bin/codelldb'),
-    args = { "--port", "13000" },
-  },
+    type = "server",
+    port = 13000,
+    executable = {
+        command = vim.fn.expand('~/.local/share/nvim/mason/bin/codelldb'),
+        args = { "--port", "13000" },
+    },
 }
 
 dap.configurations.c = {
-  {
-    type = "codelldb",
-    request = "launch",
-    program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd()..'/main', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-  }
+    {
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/main', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+    }
 }
 
 dap.configurations.cpp = dap.configurations.c
 
 dap.adapters.delve = {
-  type = 'server',
-  port = 38697,
-  executable = {
-    command = 'dlv',
-    args = {'dap', '-l', '127.0.0.1:38697'},
-  }
+    type = 'server',
+    port = 38697,
+    executable = {
+        command = 'dlv',
+        args = { 'dap', '-l', '127.0.0.1:38697' },
+    }
 }
 
 dap.adapters.go = {
-  type = 'executable';
-  command = 'node';
-  args = {os.getenv('HOME') .. '/vscode-go/extension/dist/debugAdapter.js'};
+    type = 'executable',
+    command = 'node',
+    args = { os.getenv('HOME') .. '/vscode-go/extension/dist/debugAdapter.js' },
 }
 
 local function filtered_pick_process()
-  local opts = {}
-  vim.ui.input(
-    { prompt = "Search by process name (lua pattern), or hit enter to select from the process list: " },
-    function(input)
-      opts["filter"] = input or ""
-    end
-  )
-  return require("dap.utils").pick_process(opts)
+    local opts = {}
+    vim.ui.input(
+        { prompt = "Search by process name (lua pattern), or hit enter to select from the process list: " },
+        function(input)
+            opts["filter"] = input or ""
+        end
+    )
+    return require("dap.utils").pick_process(opts)
 end
 
 local function get_debug_process_id()
-    local pid = vim.fn.system("pidof 'debugapp'")
+    local pid = vim.fn.system("pidof 'app_debug'")
     if tonumber(pid) == nil then
         return require("dap.utils").pick_process()
     else
@@ -59,32 +59,49 @@ local function get_debug_process_id()
     end
 end
 
+local function get_file()
+    os.execute("./debug")
+    local isApp = vim.fn.executable("./app_debug")
+    if isApp == 0 then
+        return require("dap.utils").pick_file()
+    else
+        return "${file}"
+    end
+end
+
 dap.configurations.go = {
     {
         type = "delve",
-        name = "Attach",
+        name = "Attach to process",
         request = "attach",
         mode = "local",
         processId = get_debug_process_id
+    },
+    {
+        type = "delve",
+        name = "Run (generated from 'debug' script)",
+        request = "launch",
+        program = get_file,
+
     }
 }
 
 local function widgets()
-  local widgets = require('dap.ui.widgets');
-  local sidebar = widgets.sidebar(widgets.scopes);
-  sidebar.open();
+    local widgets = require('dap.ui.widgets');
+    local sidebar = widgets.sidebar(widgets.scopes);
+    sidebar.open();
 end
 
 dapui.setup()
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+    dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 
 vim.keymap.set("n", "<leader>db", ':DapToggleBreakpoint<CR>')
--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,45 +1,45 @@
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    -- 'jdtls',
-    -- 'gopls',
-    -- 'golines',
-    -- 'goimports',
-    -- 'eslint-lsp',
-    -- 'lua-language-server',
-    -- 'pyright',
-    -- 'python-lsp-server',
-    -- 'rust-analyzer',
-    -- 'typescript-language-server',
-    -- 'codelldb',
-    -- 'clang-format',
-    -- 'clangd',
-  },
-  -- automatic_installation = true,
+require("mason").setup({
+    ensure_installed = {
+        'jdtls',
+        'gopls',
+        'golines',
+        'goimports',
+        'eslint-lsp',
+        'lua-language-server',
+        'pyright',
+        'python-lsp-server',
+        'rust-analyzer',
+        'typescript-language-server',
+        'codelldb',
+        'clang-format',
+        'clangd',
+    },
+    automatic_installation = true,
 })
+require("mason-lspconfig").setup()
 
 -- local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('lspsaga').setup({
-  -- code_action_icon = "💡",
-  symbol_in_winbar = {
-    in_custom = false,
-    enable = true,
-    separator = ' > ',
-    show_file = true,
-    file_formatter = ""
-  },
-  lightbulb = {
-    enable = false,
-    enable_in_insert = false,
-    sign = true,
-    sign_priority = 40,
-    virtual_text = false,
-  },
-  hover = {
-    open_link = 'O',
-  },
+    -- code_action_icon = "💡",
+    symbol_in_winbar = {
+        in_custom = false,
+        enable = true,
+        separator = ' > ',
+        show_file = true,
+        file_formatter = ""
+    },
+    lightbulb = {
+        enable = false,
+        enable_in_insert = false,
+        sign = true,
+        sign_priority = 40,
+        virtual_text = false,
+    },
+    hover = {
+        open_link = 'O',
+    },
 })
 
 vim.keymap.set("n", "<leader>mf", ":Lspsaga lsp_finder<CR>", { silent = true })
@@ -47,27 +47,27 @@ vim.keymap.set('n', '<leader><A-k>', ':Lspsaga hover_doc<CR>', { silent = true }
 vim.keymap.set("n", "<leader>mr", ":Lspsaga rename<CR>", { silent = true })
 vim.keymap.set("n", "<leader>ma", ":Lspsaga code_action<CR>", { silent = true })
 vim.keymap.set("n", "<leader>ma", ":Lspsaga code_action<CR>", { silent = true })
-vim.keymap.set({"n", "t"}, "<A-CR>", ":Lspsaga term_toggle<CR>", { silent = true })
+vim.keymap.set({ "n", "t" }, "<A-CR>", ":Lspsaga term_toggle<CR>", { silent = true })
 
 require("lspconfig").lua_ls.setup({
-  capabilities = capabilities,
+    capabilities = capabilities,
 })
 require("lspconfig").tsserver.setup({
-  capabilities = capabilities,
+    capabilities = capabilities,
 })
 -- require("lspconfig").jdtls.setup({})
 require("lspconfig").clangd.setup({
-  capabilities = capabilities,
+    capabilities = capabilities,
 })
 require("lspconfig").gopls.setup({
-  capabilities = capabilities,
-  settings = {
-    gopls = {
-      completeUnimported = false,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      }
-    }
-  }
+    capabilities = capabilities,
+    settings = {
+        gopls = {
+            completeUnimported = false,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+        }
+    },
 })
